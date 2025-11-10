81c77eec24d58671cd8bc68ded081856ae83758c
marco <marcoadrianzambra@gmail.com>
2025-11-10 16:35:00 -0500

    fix: create account and I already have and accountn button now working!

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
