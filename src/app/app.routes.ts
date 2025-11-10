c67f902dce1dd405dfa8b9c9783cd19bb7e3bb62
marco <marcoadrianzambra@gmail.com>
2025-11-10 16:16:05 -0500

    feat: app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login';
+import { RegisterComponent } from './auth/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+	// add more routes here
+];
